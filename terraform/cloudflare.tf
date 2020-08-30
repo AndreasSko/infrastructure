cf57c27b90d9c920dad92ffdedf754bab8c687cd
Andreas Skorczyk <me@andreas-sk.de>
2020-08-30 19:24:38 +0200

    Deploy bthl-tours Flarum forum

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -6,6 +6,14 @@ resource "cloudflare_record" "hetzner" {
   ttl     = 120
 }
 
+resource "cloudflare_record" "bthl_tours" {
+  zone_id = lookup(data.cloudflare_zones.andreas_sk.zones[0], "id")
+  name    = "bthl-tours.andreas-sk.de"
+  value   = hcloud_server.andreas_sk.ipv4_address
+  type    = "A"
+  proxied = true
+}
+
 data "cloudflare_zones" "andreas_sk" {
   filter {
     name = "andreas-sk.de"
