7ad816ee4b4bfa5cc7bacc30d0b3fe46e23ea724
Andreas Skorczyk <me@andreas-sk.de>
2020-09-03 08:55:55 +0200

    Update Flarum hostname
    Back to bethel-recommendations.andreas-sk.de

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -8,7 +8,7 @@ resource "cloudflare_record" "hetzner" {
 
 resource "cloudflare_record" "bthl_tours" {
   zone_id = lookup(data.cloudflare_zones.andreas_sk.zones[0], "id")
-  name    = "bthl-tours.andreas-sk.de"
+  name    = "bethel-recommendations.andreas-sk.de"
   value   = hcloud_server.andreas_sk.ipv4_address
   type    = "A"
   proxied = true
