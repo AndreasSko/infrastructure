c0073d0621e93928aad0d443029f35c2d5a31868
Andreas Skorczyk <me@andreas-sk.de>
2020-10-03 16:12:42 +0200

    WORKSTATION_SSH_PUB_KEY using secrets
    So we don't need to have the public key stored in
    git.

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -11,5 +11,4 @@ variable "ssh_pub_key" {
 variable "workstation_ssh_pub_key" {
   type        = string
   description = "The public key that will be used to access the server from the workstation"
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+MngXuYuh/ZrojLf3oANA94Wq2waFUvO3gW55SEKpnSK58nsqGGJz5CzrNdSmA5DrdEzuj86PyAIio67PjVfHz3q6bjC46P8upyk6jmGVwEMpqcfHcyTIo1faCoBai7GwciHYbemrKSK+YJNNKY9VYz998imyOPQKv8yk7G0bJlAEnj7RlR90I8FacTP/CQuD7pcMp3bFrvxRNKtBUVzBTqlJ/3EbCBuhRr1YwFhohxkmUzD9ic4FY3BIpqrbaMhEGAz+atrf7WUsw5V5WqV5ldk+3wCB0b3kwZ35RSHZCLdosgRSM9m+GdiVwabuXwtaC+zooGHUH8MQbpMKQ/PWT/E/+r537ptGd52SiGbSkSKLCiBekuEpncdfCQ6GbSJ3sj6uAtqwyGEgnfsoQbb863FxPAfrN7xtOuvjDHzbUkZ9WDPBtzQXc0BUS5Q8em01bj09skkGpe+uCZcsCw7NiVh3J4TmfRZZn5Fu+H4PKpj8sJB5zGaH61aKHVlf+ZM= askorczyk@Andreass-MacBook-Pro.local"
 }
