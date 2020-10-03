2b665814b4acb95c549e033b8c8f2777751ef369
Andreas Skorczyk <me@andreas-sk.de>
2020-10-03 16:04:06 +0200

    Remove Flarum as not needed anymore
    Hosted somewhere else now

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -6,23 +6,6 @@ resource "cloudflare_record" "hetzner" {
   ttl     = 120
 }
 
-resource "cloudflare_record" "bthl_tours" {
-  zone_id = lookup(data.cloudflare_zones.andreas_sk.zones[0], "id")
-  name    = "bethel-recommendations.andreas-sk.de"
-  value   = hcloud_server.andreas_sk.ipv4_address
-  type    = "A"
-  proxied = true
-}
-
-
-resource "cloudflare_record" "flarum_rehearse" {
-  zone_id = lookup(data.cloudflare_zones.andreas_sk.zones[0], "id")
-  name    = "flarum-rehearse2.andreas-sk.de"
-  value   = hcloud_server.andreas_sk.ipv4_address
-  type    = "A"
-  proxied = true
-}
-
 resource "cloudflare_record" "nextcloud" {
   zone_id = lookup(data.cloudflare_zones.andreas_sk.zones[0], "id")
   name    = "nextcloud.andreas-sk.de"
