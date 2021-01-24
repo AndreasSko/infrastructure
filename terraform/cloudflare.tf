ab019c5a12e4e46da494044486dfcf33b0af62c9
Andreas Skorczyk <me@andreas-sk.de>
2021-01-24 10:45:25 +0100

    TF: Feedbin hostnames

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -22,6 +22,31 @@ resource "cloudflare_record" "monitoring" {
   proxied = true
 }
 
+# Feedbin
+resource "cloudflare_record" "feedbin" {
+  zone_id = lookup(data.cloudflare_zones.andreas_sk.zones[0], "id")
+  name    = "feedbin.andreas-sk.de"
+  value   = hcloud_server.andreas_sk.ipv4_address
+  type    = "A"
+  proxied = true
+}
+
+resource "cloudflare_record" "camo-feedbin" {
+  zone_id = lookup(data.cloudflare_zones.andreas_sk.zones[0], "id")
+  name    = "camo-feedbin.andreas-sk.de"
+  value   = hcloud_server.andreas_sk.ipv4_address
+  type    = "A"
+  proxied = true
+}
+
+resource "cloudflare_record" "extract-feedbin" {
+  zone_id = lookup(data.cloudflare_zones.andreas_sk.zones[0], "id")
+  name    = "extract-feedbin.andreas-sk.de"
+  value   = hcloud_server.andreas_sk.ipv4_address
+  type    = "A"
+  proxied = true
+}
+
 data "cloudflare_zones" "andreas_sk" {
   filter {
     name = "andreas-sk.de"
