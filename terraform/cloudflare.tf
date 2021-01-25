5f37f0287b3b549695aa1bf9d0530923fa74b200
Andreas Skorczyk <me@andreas-sk.de>
2021-01-25 15:02:22 +0100

    Feedbin: Sendgrid DNS records
    Preparation to receive emails for Feedbin.

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -31,6 +31,27 @@ resource "cloudflare_record" "feedbin" {
   proxied = true
 }
 
+resource "cloudflare_record" "feedbin-sendgrid-mx1" {
+  zone_id = lookup(data.cloudflare_zones.andreas_sk.zones[0], "id")
+  name    = "em669.feedbin.andreas-sk.de"
+  value   = "mx.sendgrid.net."
+  type    = "MX"
+}
+
+resource "cloudflare_record" "feedbin-sendgrid-txt1" {
+  zone_id = lookup(data.cloudflare_zones.andreas_sk.zones[0], "id")
+  name    = "em669.feedbin.andreas-sk.de"
+  value   = "v=spf1 include:sendgrid.net ~all"
+  type    = "TXT"
+}
+
+resource "cloudflare_record" "feedbin-sendgrid-txt2" {
+  zone_id = lookup(data.cloudflare_zones.andreas_sk.zones[0], "id")
+  name    = "m1._domainkey.feedbin.andreas-sk.de"
+  value   = "k=rsa; t=s; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDDDmq/dVRkjJ5UfbZLlK4pMxkuifhrgpG4zkm4T1VwAuyUiSQFI7V5qDM9lDRnHEl6kWy2+UsOSAVzVYynawWLd28AkFl0a6i/oK79PQZFCAy+Dtfbjca8FBXVdX/10c7oFToFmu/XeuCYlZ4cCtTi1D7TQy8T+EXMVd+YQOXu3QIDAQAB"
+  type    = "TXT"
+}
+
 resource "cloudflare_record" "camo-feedbin" {
   zone_id = lookup(data.cloudflare_zones.andreas_sk.zones[0], "id")
   name    = "camo-feedbin.andreas-sk.de"
