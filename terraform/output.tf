f8bff5f1d77b43adb8002b51986b0e528a816101
Andreas Skorczyk <me@andreas-sk.de>
2021-12-07 11:16:43 +0100

    Cutover to 1Blu Server

--- terraform/output.tf
+++ terraform/output.tf
@@ -4,6 +4,6 @@ output "server_ipv4" {
 }
 
 resource "local_file" "ansible_inventory" {
-  sensitive_content = templatefile("./templates/hosts.yml.tmpl", { IP = hcloud_server.andreas_sk.ipv4_address })
+  sensitive_content = templatefile("./templates/hosts.yml.tmpl", { IP = cloudflare_record.one-blu.hostname })
   filename          = "./../ansible/hosts.yml"
 }
