50ef4193673ba99c146aec1edbb721b803d4fa81
Andreas Skorczyk <me@andreas-sk.de>
2022-10-01 12:37:44 +0200

    Fix Oracle instance

--- terraform/oracle.tf
+++ terraform/oracle.tf
@@ -1,19 +1,25 @@
 resource "oci_core_instance" "andreas-sk-de" {
-  availability_domain = "AD-1"
+  availability_domain = "FTem:EU-FRANKFURT-1-AD-3"
   compartment_id      = var.oracle_compartment_id
-  shape               = "VM.Standard.A1.Flex"
+  display_name        = "andreas-sk.de"
 
-  display_name   = "andreas-sk.de"
-  hostname_label = "andreas-sk-de"
+  shape = "VM.Standard.A1.Flex"
+  shape_config {
+    memory_in_gbs = 24
+    ocpus         = 4
+  }
 
   source_details {
     boot_volume_size_in_gbs = 200
+    boot_volume_vpus_per_gb = 120
     source_type             = "image"
     source_id               = data.oci_core_image.ubuntu_2204.image_id
   }
 
   create_vnic_details {
     assign_public_ip = true
+    hostname_label   = "andreas-sk-de"
+    subnet_id        = "ocid1.subnet.oc1.eu-frankfurt-1.aaaaaaaae2pkrlzidl5v6dpnvho4imhu44ca72fdes6ezyxvl43cdnhmk2oq"
   }
 
   metadata = {
@@ -22,5 +28,5 @@ resource "oci_core_instance" "andreas-sk-de" {
 }
 
 data "oci_core_image" "ubuntu_2204" {
-  image_id = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa7wq4opozz63gwzrolqmalwadtckpke5ehhxh634myjquvwlzetyq"
+  image_id = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaj6g2lci5ed7nfhk46olwkhmwkzrobyo3jntnhkk7fnm2vqflorna"
 }
