056f2feff2084452b9eaad090dda077a2db69049
Andreas Skorczyk <me@andreas-sk.de>
2022-10-01 21:21:20 +0200

    Only apply Ansible on Oracle machine

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,6 @@
 resource "local_file" "ansible_inventory" {
   sensitive_content = templatefile("./templates/hosts.yml.tmpl", {
     addrs = [
-      cloudflare_record.one-blu.hostname,
       cloudflare_record.oracle.hostname,
     ]
   })
